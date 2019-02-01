db09584771ba24dd2d8f8de73e70b8e7f6376c35
Victor Procure <victor@victorprocure.co>
2019-01-31 23:04:21 -0700

    styling fix

--- src/core/Physics/BarometricPressure.test.ts
+++ src/core/Physics/BarometricPressure.test.ts
@@ -8,7 +8,7 @@ const expect = chai.expect;
 describe('Barometric Pressure Calculator', () => {
 
     it('should be able to calculate pressure correctly', () => {
-        expect(BarometricPressure.GetPressure(101325, 288.15, 2500)).to.be.closeTo(74682.53, 0.01);
-        expect(BarometricPressure.GetPressure(760, 288.15, 100)).to.be.closeTo(751.03, 0.01);
+        expect(BarometricPressure.getPressure(101325, 288.15, 2500)).to.be.closeTo(74682.53, 0.01);
+        expect(BarometricPressure.getPressure(760, 288.15, 100)).to.be.closeTo(751.03, 0.01);
     });
 });
--- src/core/Physics/BarometricPressure.ts
+++ src/core/Physics/BarometricPressure.ts
@@ -11,25 +11,25 @@ export abstract class BarometricPressure {
      * @param temperature Temperature of air in Kelvin
      * @param altitude Altitude to calculate in meters
      */
-    public static GetPressure(pressureAtSeaLevel: number, temperature: number, altitude: number): number {
+    public static getPressure(pressureAtSeaLevel: number, temperature: number, altitude: number): number {
         if (altitude < 11000) {
-            return this.GetPressureLapseRateNotZero(pressureAtSeaLevel, temperature, altitude);
+            return this.getPressureLapseRateNotZero(pressureAtSeaLevel, temperature, altitude);
         }
 
         if (altitude <= 20000) {
-            return this.GetPressureLapseRateZero(pressureAtSeaLevel, temperature, altitude);
+            return this.getPressureLapseRateZero(pressureAtSeaLevel, temperature, altitude);
         }
 
         return NaN;
     }
 
-    private static GetPressureLapseRateNotZero(pressureAtSeaLevel: number, temperature: number, altitude: number) {
+    private static getPressureLapseRateNotZero(pressureAtSeaLevel: number, temperature: number, altitude: number) {
         return pressureAtSeaLevel * Math.pow(temperature / (temperature + (this._temperatureLapseRate * (altitude - this._heightOfBottomLayer))), (GravitationalAccelarationOnEarth * MolarMassEarthAir) / (UniversalGasConstantForAir * this._temperatureLapseRate));
     }
 
-    private static GetPressureLapseRateZero(pressureAtSeaLevel: number, temperature: number, altitude: number) {
+    private static getPressureLapseRateZero(pressureAtSeaLevel: number, temperature: number, altitude: number) {
         var heightOfBottomLayer = 11000;
-        var pressureAtLapseRateNotZero = this.GetPressureLapseRateNotZero(pressureAtSeaLevel, temperature, heightOfBottomLayer);
+        var pressureAtLapseRateNotZero = this.getPressureLapseRateNotZero(pressureAtSeaLevel, temperature, heightOfBottomLayer);
         var temperatureLapseRate = temperature + (11000 * (this._temperatureLapseRate));
 
         return pressureAtLapseRateNotZero * Math.exp(((-GravitationalAccelarationOnEarth) * MolarMassEarthAir * (altitude - heightOfBottomLayer)) / (UniversalGasConstantForAir * temperatureLapseRate))
